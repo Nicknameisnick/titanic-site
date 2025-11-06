c980869ed807ae83c611cf03af41bdc885fb5f5b
mdew-05 <matthijsdewolff@gmail.com>
2025-11-06 14:25:07 +0100

    Update titanic site.py

--- titanic site.py
+++ titanic site.py
@@ -58,10 +58,14 @@ elif pagina == "Titanic case 1e poging":
     st.header("Eerste poging")
     st.write("In de eerste poging hebben wij een voorspelling gemaakt op basis van een paar variabelen, er is hier geen gebruik gemaakt van een machine learning model")
 
+    st.subheader("Variabelen gebruikt voor de eerste poging:")
+    st.image("https://github.com/Nicknameisnick/titanic-site/blob/main/1e%20poging%20train%20set.png")
+
 
 elif pagina == "Titanic case verbetering (2e poging)":
     st.title("Titanic case verbetering (2e poging)")
     st.write("")
+
 
 
 
