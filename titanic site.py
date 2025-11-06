e3734392e91677c71fdf7a56823b7d1c461296c1
mdew-05 <matthijsdewolff@gmail.com>
2025-11-06 14:19:11 +0100

    Update titanic site.py

--- titanic site.py
+++ titanic site.py
@@ -50,13 +50,19 @@ if pagina == "Titanic case intro":
     )
 
 
+
 elif pagina == "Titanic case 1e poging":
     st.title("Titanic case 1e poging")
-    st.write("")
+
+    
+    st.header("Eerste poging")
+    st.write("In de eerste poging hebben wij een voorspelling gemaakt op basis van een paar variabelen, er is hier geen gebruik gemaakt van een machine learning model")
+
 
 elif pagina == "Titanic case verbetering (2e poging)":
     st.title("Titanic case verbetering (2e poging)")
     st.write("")
+
 
 
 
