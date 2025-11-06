6438fbcbf72e378868c4b8b44a003dfd153651f4
mdew-05 <matthijsdewolff@gmail.com>
2025-11-06 13:44:49 +0100

    Update titanic site.py

--- titanic site.py
+++ titanic site.py
@@ -12,7 +12,11 @@ import folium
 import numpy as np 
 import streamlit as st 
 from sklearn.ensemble import RandomForestRegressor
-import plotly.graph_objects as go
 from folium.plugins import MarkerCluster
-
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
+import plotly.graph_objects as go
 
