e5fb6b0024324e45a73afc690f3c05e6a86ed8a6
mdew-05 <matthijsdewolff@gmail.com>
2025-11-06 15:10:22 +0100

    Update titanic site.py

--- titanic site.py
+++ titanic site.py
@@ -57,7 +57,9 @@ if pagina == "Titanic case intro":
     
     We laten zien welke keuzes we maakten bij het opstellen van voorspellingen en wat we daarvan hebben geleerd.
     """)
-  
+
+    st.sidebar.markdown("---")
+    
     col1, col2 = st.columns([1.2, 1])  # verhouding: iets meer ruimte voor tekst (links)
 
     # Linkerkolom — tekstblok
@@ -285,6 +287,7 @@ df_cleaned['Parch'].fillna(df_cleaned['Parch'].median(), inplace=True)
     with tab5:
         st.header("Conclusies en eindscore")
         st.write("Conclusies en de eindscore van het model.")
+
 
 
 
